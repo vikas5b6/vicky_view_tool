869ce34727e1755c1440a3f2b0a4fd8df23b7365
Vikas <d.vikas5b6@gmail.com>
2018-11-07 23:15:33 -0500

    Initial commit

---
+++ lib/vicky_view_tool.rb
@@ -0,0 +1,7 @@
+require "vicky_view_tool/version"
+require "vicky_view_tool/renderer"
+
+module VickyViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vicky_view_tool/version.rb
@@ -0,0 +1,3 @@
+module VickyViewTool
+  VERSION = "0.1.0"
+end
